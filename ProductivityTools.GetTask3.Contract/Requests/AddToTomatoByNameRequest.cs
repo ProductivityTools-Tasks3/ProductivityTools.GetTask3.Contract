1f2014026d8bcd8d7c204eeed418125602904ca0
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-07-07 23:07:42 +0200

    move from other project

---
+++ ProductivityTools.GetTask3.Contract/Requests/AddToTomatoByNameRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.GetTask3.Contract.Requests
+{
+    public class AddToTomatoByNameRequest
+    {
+        public int ParentId { get; set; }
+        public string TaskName { get; set; }
+    }
+}
---
+++ ProductivityTools.GetTask3.Contract/Requests/GetRootRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.GetTask3.Contract.Requests
+{
+    public class GetRootRequest
+    {
+        public int? ElementId { get; set; }
+        public string Path { get; set; }
+    }
+}
